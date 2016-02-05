1be122a9753492a71f541e333f95fef9cdae5dbc
Eric Suarez <egs104@gmail.com>
2016-02-04 19:39:26 -0500

    finished up to part 5

---
+++ ParseChat/ChatCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatCell.swift
+//  ParseChat
+//
+//  Created by Eric Suarez on 2/4/16.
+//  Copyright © 2016 Eric Suarez. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
